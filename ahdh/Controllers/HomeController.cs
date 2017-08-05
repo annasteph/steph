11219c837ebb03058fbe16801cf28bdffb159aa4
unknown <pt808@CSLAB102.CSSERVER.local>
2017-08-05 18:58:49 +0800

    First revision

---
+++ ahdh/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ahdh.Models;
+
+namespace ahdh.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            Information i = new Information();
+            return View(i.Accounts);
+        }
+        public ActionResult Add()
+        {
+            
+            return View();
+        }
+        public ActionResult Save(Account acc)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ ahdh/Models/Information.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ahdh.Models
+{
+    public class Information
+    {
+        public List<Account> Accounts;
+
+        public Information() 
+        {
+            Accounts = new List<Account>();
+            Accounts.Add(new Account { Id = 1, Name = "My name1", Age = 12 });
+            Accounts.Add(new Account { Id = 2, Name = "My name2", Age = 12 });
+            Accounts.Add(new Account { Id = 3, Name = "My name3", Age = 12 });
+        }
+    }
+}
